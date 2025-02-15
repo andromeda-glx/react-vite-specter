f130d72c145e37de44708931d97d79f5f9db2ece
Faran <faran.7j99@gmail.com>
2025-02-15 16:07:24 +0330

    style 404 not found page

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,5 +1,11 @@
+// import img from "../assets/specter not found.png"
+
 export default function PageNotFound() {
-  return (
-    <div>PageNotFound</div>
-  )
+    return (
+        <div className="flex flex-col justify-center items-center">
+            <div className={`w-[500px] h-[500px] bg-[url(./assets/specter-not-found.png)] bg-contain opacity-80`}>
+            </div>
+            <h1 className="text-4xl font-bold">404 Page Not Found</h1>
+        </div>
+    )
 }
