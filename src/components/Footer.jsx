cbed33f5c577e41d56324ccec7f53c0763208259
Faran <faran.7j99@gmail.com>
2025-02-15 15:27:15 +0330

    fix footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 export default function Footer({lightMode}) {
   return (
-    <footer className={`${lightMode ? "bg-blue-600 " : "bg-blue-900"} text-white p-5 transition-colors`}>
+    <footer className={`${lightMode ? "bg-blue-600 " : "bg-blue-900"} text-white p-5 transition-colors text-center text-sm`}>
         &copy; SPECTER 2025 All rights reserved.
     </footer>
   )
